8607428a08d1f5111eb59cac2e0ed97b3cfd50b2
Laine Moura <lainermoura@gmail.com>
2022-09-10 14:43:30 -0300

    limpeza de código

--- src/model/ShowsSignupDTO.ts
+++ src/model/ShowsSignupDTO.ts
@@ -1,13 +1,13 @@
 export interface ShowSignupDTO {
-   bandId: string,
-   weekday: SHOW_DAY,
-   startTime: number,
-   endTime: number
+    bandId: string,
+    weekday: SHOW_DAY,
+    startTime: number,
+    endTime: number
 };
 
 export interface ShowModel {
-id:string,
-   weekday: SHOW_DAY,
+    id: string,
+    weekday: SHOW_DAY,
     startTime: number,
     endTime: number,
     bandId: string
@@ -15,32 +15,10 @@ id:string,
 
 export enum SHOW_DAY {
     FRIDAY = "FRIDAY",
-    SATURDAY = "SATURDAY", 
-    SUNDAY = "SUNDAY" 
-
+    SATURDAY = "SATURDAY",
+    SUNDAY = "SUNDAY"
 }
 
 export interface ShowDetailDTO {
     weekday: string;
-  }
-
-  
-
-export enum time {
-    eigthAM = "08:00",
-    nineAM = "09:00",
-    tenAM = "10:00",
-    elevenAM = "11:00",
-    midday = "12:00",
-    onePM = "13:00",
-    twoPM = "14:00",
-    threePM = "15:00",
-    fourPM = "16:00",
-    fivePM = "17:00",
-    sixPM = "18:00",
-    sevenPM = "19:00",
-    eightPM = "20:00",
-    ninePM = "21:00",
-    tenPM = "22:00",
-    elevenPM = "23:00",
 }
